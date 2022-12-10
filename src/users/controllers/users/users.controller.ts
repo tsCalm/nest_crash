4456fad92c0872bf5dac3a2b79a3cdd2029820f6
tsCalm <dlatkdals93@gmail.com>
2022-12-10 22:48:04 +0900

    update: user module

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -23,12 +23,14 @@ export class UsersController {
   }
 
   @Get(':id')
+  // ParseIntPipe가 없으면 string 타입
   getUser(@Param('id', ParseIntPipe) id: number) {
     console.log(typeof id);
     return id;
   }
 
   @Post('create')
+  // 개별적인 validationPipe
   @UsePipes(new ValidationPipe())
   createUser(@Body() userData: CreateUserDto) {
     console.log(userData);
