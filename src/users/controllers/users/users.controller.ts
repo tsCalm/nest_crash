844107914999a38243bd01c80c684818d8dc662f
tsCalm <dlatkdals93@gmail.com>
2022-12-11 01:06:01 +0900

    feat: users module guard

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -8,10 +8,12 @@ import {
   ParseIntPipe,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/createUser.dto';
+import { AuthGuard } from 'src/users/guards/auth/auth.guard';
 import { ValidateCreateUserPipe } from 'src/users/pipes/validate-create-user/validate-create-user.pipe';
 import { UsersService } from 'src/users/services/users/users.service';
 
@@ -20,6 +22,7 @@ export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getUsers(@Query('sortBy') sortBy: string) {
     console.log(sortBy);
     return this.userService.fetchUsers();
@@ -37,7 +40,6 @@ export class UsersController {
   // 개별적인 validationPipe
   @UsePipes(new ValidationPipe())
   createUser(@Body(ValidateCreateUserPipe) userData: CreateUserDto) {
-    console.log(typeof userData.age);
     return this.userService.createUser(userData);
   }
 }
---
+++ src/users/guards/auth/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest() as Request;
+    console.log(req.headers);
+    return false;
+    // return true
+  }
+}
